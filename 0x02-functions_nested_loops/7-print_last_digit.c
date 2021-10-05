c97c80e888bc750798e88361e1fed4d2decdba45
salvador <3940@holbertonschool.com>
2021-10-05 10:29:15 -0700

    a

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+/**
+ * print_last_digit - Prints the last digit of an integer number
+ * @n
+ *
+ * Return: The last number of the number
+ */
+int print_last_digit(int n)
+{
+	int lastd;
+	lastd = n % 10;
+	return (lastd);
+}
