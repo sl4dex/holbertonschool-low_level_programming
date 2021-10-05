5c8e1d6b3cb2be04cd63c5b4f90cf91a649f3d51
salvador <3940@holbertonschool.com>
2021-10-05 10:31:13 -0700

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,13 @@
 /**
  * print_last_digit - Prints the last digit of an integer number
- * @n
+ * @n: The number that we are going to substract its first number
  *
  * Return: The last number of the number
  */
 int print_last_digit(int n)
 {
 	int lastd;
+
 	lastd = n % 10;
 	return (lastd);
 }
