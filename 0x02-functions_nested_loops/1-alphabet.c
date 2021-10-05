3ccf897e2f39cd68f8c6190d9c29c10e3bd0b87b
salvador <3940@holbertonschool.com>
2021-10-05 08:35:39 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+void print_alphabet()
 {
-    print_alphabet();
-    return (0);
-}
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+	return;
+ }
+
