f06d7d07e69a3552b42382270ca8e2b94c53857d
salvador <3940@holbertonschool.com>
2021-10-05 09:00:53 -0700

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,9 @@
+/**
+ * _islower - checks if a character is in lowercase
+ * @c : the character to be checked
+ *
+ * Return: 1 if the character is lowercase. 0 otherwise
+ */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
