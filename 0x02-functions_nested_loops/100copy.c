346d5f0f06faa3122ea484c7b6104fb5b2f08019
salvador <3940@holbertonschool.com>
2021-10-06 13:59:48 -0700

    a

--- 0x02-functions_nested_loops/100copy.c
+++ 0x02-functions_nested_loops/100copy.c
@@ -6,13 +6,17 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
+		/* prints the columns*/
 		for (col = 0; col <= n; col++)
 		{
+			/* prints each cell of the row */
 			for (row = 0; row <= n; row++)
 			{
 				result = row * col;
+				/* if the cell is 0-9 ... */
 				if (result <= 9)
    				{
+					/* ... and its not in the first column */
 					if (row >= 1)
 					{
 						_putchar(' ');
@@ -21,9 +25,13 @@ void print_times_table(int n)
 					}
 					_putchar('0' + result);
 
+					/* if its not the last row */
 					if (row < n)
+					{
 						_putchar(',');
+					}
 				}
+				/* if the cell is 10-99 ... */
 				else if (result >= 10 && result <= 99)
 				{
 					i = result / 10;
@@ -33,8 +41,12 @@ void print_times_table(int n)
 					_putchar('0' + i);
 					_putchar('0' + j);
 					if (row < n)
+					{
 						_putchar(',');
+					}
 				}
+
+				/* else, we expecting row has three digits */
 				else
 				{
 					_putchar(' ');
@@ -44,8 +56,11 @@ void print_times_table(int n)
 					_putchar('0' + i);
 					_putchar('0' + j);
 					_putchar('0' + k);
-					if (row < n)
-						_putchar(',');
+				if (row < n)
+				{
+					_putchar(',');
+				
+				}
 				}
 			}
 			_putchar(10);
