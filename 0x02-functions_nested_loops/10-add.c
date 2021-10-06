ab7cd3dacfd6000f5923ba773d86f3d6640e0920
salvador <3940@holbertonschool.com>
2021-10-06 10:10:17 -0700

    a

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,11 @@
+/**
+ * add - sums two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of both numbers
+ */
 int add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
