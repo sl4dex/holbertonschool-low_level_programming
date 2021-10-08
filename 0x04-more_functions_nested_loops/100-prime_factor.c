d0da2f537fc3bd24283848df5ad437f769a907e7
salvador <3940@holbertonschool.com>
2021-10-08 09:44:25 -0700

    a

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-int main() {
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
 	long int num, div;
+
 	num = 612852475143;
-	
-	while(div < num)
+	while (div < num)
 	{
 		for (div = 2; div < num; div++)
 			if (num % div == 0)
 				num = num / div;
 	}
 	printf("%ld\n", num);
-	return 0;
+	return (0);
 }
