af03a7c606d5dc28e2612d0bb1cc899235b9019f
salvador <3940@holbertonschool.com>
2021-10-12 10:54:46 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout
+ * @str: pointer to string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(*(str + i));
+	_putchar(10);
+}
