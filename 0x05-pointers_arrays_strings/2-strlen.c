a5cdd40992c4ace5f0afa8c5c89e082b467e5a78
salvador <3940@holbertonschool.com>
2021-10-12 09:22:17 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer to string (array of chars)
+ *
+ * Return: String size
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
