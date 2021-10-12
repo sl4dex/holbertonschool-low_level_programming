27526a4b48590cc4bd0bbf1eef979222d1e1d579
salvador <3940@holbertonschool.com>
2021-10-12 14:41:30 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != 0; i += 2)
+	for (i = 0; str[i] != 0; i++)
 	{
-		_putchar(str[i]);
 	}
+	for (i = 0; str[i] <= i; i += 2)
 	_putchar(10);
 }
