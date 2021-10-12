e755f71b91c900483af2b4d5bff33eea30bdfdfe
salvador <3940@holbertonschool.com>
2021-10-12 11:09:55 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: pointer to string
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	for (j = i; j >= 0; j--)
+		_putchar(*(s + j));
+	_putchar(10);
+}
