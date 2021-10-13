7e26ac79655179a9d2e4cc652992b7f04e25dd95
salvador <3940@holbertonschool.com>
2021-10-12 17:53:04 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * puts2 - prints every other character of a string
  * @str: string 
  */
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; str[i] != 0; i += 2)
+	for (i = 0; str[i] != 0; i++)
 	{
-		_putchar(str[i]);
+	}
+	for (j = 0; j <= i; j += 2)
+	{
+		if (str[j] != 0)
+			_putchar(str[j]);
 	}
 	_putchar(10);
 }
