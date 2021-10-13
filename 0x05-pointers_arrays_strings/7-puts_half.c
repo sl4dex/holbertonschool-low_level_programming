d55260f1ef7eaa05d23abccb1d59d7e4a60df5fc
salvador <3940@holbertonschool.com>
2021-10-12 18:21:25 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,6 +9,14 @@ void puts_half(char *str)
 	}
 	for (j = 0; str[j] != 0; j++)
 	{
+		if (i % 3 == 0)
+		{
+			if (j > (i / 2))
+			{
+				_putchar(str[j]);
+				break;
+			}
+		}
 		if (j >= (i / 2))
 			_putchar(str[j]);
 	}
