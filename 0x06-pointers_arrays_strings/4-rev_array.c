4c8423b6a08eab9412142e72ad0fa4937c2aa5cb
salvador <3940@holbertonschool.com>
2021-10-15 05:28:14 -0700

    added a function that reverses int arrays

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,14 +1,17 @@
+#include "main.h"
+
 void reverse_array(int *a, int n)
 {
 	int pos, aux;
-
-	for (pos = 0; pos != 0; pos++)
+	/* pasamos de nro bytes a numero de elementos ( leer sizeof() ) */
+	printf("%d and %d\n", n, n/2);
+	for (pos = 0; pos < n; pos++)
 	{
-		if (pos != n/2)
+		if (pos != n / 2)
 		{
 			aux = *(a + pos);
-			*(a + pos) = *(a + n - pos);
-			*(a + n - pos) = aux;
+			*(a + pos) = *(a + n - 1 - pos);
+			*(a + n - 1 - pos) = aux;
 		}
 		else
 		{
