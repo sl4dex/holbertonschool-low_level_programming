266a9a2ff60e7195475c925690c902b0484026df
salvador <3940@holbertonschool.com>
2021-10-18 07:29:06 -0700

    fixed ex 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	char *r;
 	int i;
-	
-	r = 0;
+
 	for (i = 0; s[i] != 0; i++)
 	{
 		if (s[i] == c)
 		{
-			r = &*(s + i - 1);
+			return (&s[i]);
 		}
 	}
-	return (r);
+	return (&s[i]);
 }
