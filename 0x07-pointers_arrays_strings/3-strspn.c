26e1d4ed4178630114339b5e3901c1535f1d9ed0
salvador <3940@holbertonschool.com>
2021-10-18 09:18:35 -0700

    added ex 3, not completed

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: string argument
+ * @accept: prefix substring
+ *
+ * Return: number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int counter;
+	int letter, pos;
+
+	 counter = 0;
+	/* for every letter in accept string */
+	for (letter = 0; accept[letter] != 0; letter++)
+	{
+		/* check if it matches with any letter of s string */
+		for (pos = 0; s[pos] != 0; pos++)
+		{
+			if (s[pos] == accept[letter])
+				counter++;
+		}
+	}
+	return (counter);
+}
