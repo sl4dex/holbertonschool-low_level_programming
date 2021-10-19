0f6fa67d553dabe1c7b41f5eb081e7067590ef5b
salvador <3940@holbertonschool.com>
2021-10-18 17:41:26 -0700

    added ex 7, the chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+/**
+ * . - 
+ * @
+ *
+ * Return: .
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int key, pos, offset;
+
+	offset = 0;
+
+	for (pos = 0; haystack[pos] != 0; pos++)
+	{
+		if (s[pos] != needle[key - offset])
+		{
+			offset++
+		}
+}
+
