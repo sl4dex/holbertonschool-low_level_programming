40efb5b7828a42dcb5a99145bc7b81afb5366b0e
salvador <3940@holbertonschool.com>
2021-10-26 09:50:11 -0700

    fixed ex 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,21 +10,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned long i, j;
+	int size1, size2, i, j;
 	char *ptr;
-
-	ptr = malloc(strlen(s1) + strlen(s2) + 1);
+	
+	for (size1 = 0; s1[size1] != 0; size1++)
+	{
+	}
+	for (size2 = 0; s2[size2] != 0; size2++)
+	{
+	}
+	ptr = malloc(size1 - 1 + size2 * sizeof(char));
 	if (!ptr)
 		return (NULL);
-	if (s1 != NULL)
-	{
-		for (i = 0; i <= strlen(s1) - 1; i++)
-			ptr[i] = s1[i];
-	}
-	if (s2 != NULL)
-	{	
-		for (j = 0; j <= strlen(s2) + 1; j++, i++)
-			ptr[i] = s2[j];
-	}
+	for (i = 0; i < size1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j <= size2; j++, i++)
+		ptr[i] = s2[j];
 	return (ptr);
 }
