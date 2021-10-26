84eb52026aef4c0c657d9acc8e78a5ae0f429da4
salvador <3940@holbertonschool.com>
2021-10-26 13:27:41 -0700

    trying ex 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		ptr[i] = malloc(width * sizeof(int));
 		if (ptr[i] == 0)
 		{
-			for (i--; i <= 0; i--)
+			for (; i <= 0; i--)
 				free(ptr[i]);
 			free(ptr);
 			return (NULL);
