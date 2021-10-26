b1e466036eae15fcc72f38b75c868f5865751838
salvador <3940@holbertonschool.com>
2021-10-26 05:34:52 -0700

    added border cases in ex 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,14 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	ptr = (char *) malloc(size * sizeof(char));
-
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
+	if (ptr == 0)
+		return (NULL);
 	return (ptr);
 }
