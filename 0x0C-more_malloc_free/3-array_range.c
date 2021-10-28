4d0939ba1ccfd3b9b57e99b35a9a0d6c47536f90
salvador <3940@holbertonschool.com>
2021-10-28 11:35:12 -0700

    added ex 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,7 +11,7 @@ int *array_range(int min, int max)
 	int *p;
 	int i;
 
-	if (min > max)
+	if (min >= max)
 		return (NULL);
 	p = malloc(sizeof(int) * (max - min) + 1);
 	for (i = 0; min <= max; i++, min++)
