6d5b924b30785d7cf2ad007e4ac777e7a7dad161
salvador <3940@holbertonschool.com>
2021-11-01 09:45:56 -0700

    added ex 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to uninitialized dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
