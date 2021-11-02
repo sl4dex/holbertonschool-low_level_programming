87213f9e188a503e8ff1d66c039ad44764f811f2
salvador <3940@holbertonschool.com>
2021-11-01 19:09:15 -0700

    added ex 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free dog pointers
+ * @d: First member
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(&(d->age));
+	free(d->owner);
+	free(d);
+}
