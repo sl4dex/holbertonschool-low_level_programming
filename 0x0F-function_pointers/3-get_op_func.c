f7c107e4d97997141618eeeef77cf242f4f20fcf
salvador <3940@holbertonschool.com>
2021-11-03 15:48:28 -0700

    fixed ex 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,11 +22,10 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0] && s[1] == 0 && s)
+		if (ops[i].op[0] == s[0] && s[1] == 0)
 			return (ops[i].f);
 		i++;
 	}
 	printf("Error\n");
 	exit(99);
-	return (NULL);
 }
