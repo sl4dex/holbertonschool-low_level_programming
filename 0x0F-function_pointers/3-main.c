dfa895b529f353749c6423f91cc61c222534f1cd
salvador <3940@holbertonschool.com>
2021-11-03 14:53:52 -0700

    added ex 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - check the code
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	int result;
@@ -12,5 +18,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
-	return (result);
+	printf("%d\n", result);
+	return (0);
+
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,61 +4,55 @@
 
 int (*get_op_func(char *s))(int, int);
 /**
-* struct op - Struct op
-*
-* @op: The operator
-* @f: The function associated
-*/
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-	/* alias for struct op */
-} op_t;
-/**
-* . -
-* @
-*
-* Return: .
+* op_add - adds 2 numbers
+* @a: first sumand
+* @b: second sumand
+* Return: added num
 */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 /**
-* . -
-* @
-*
-* Return: .
+* op_sub - substracts 2 numbers
+* @a: first num
+* @b: second num
+* Return: subtracted num
 */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
 /**
-* . -
-* @
-*
-* Return: .
+* op_mul - multiplies 2 numbers
+* @a: first multiplicand
+* @b: second multiplicand
+* Return: multiplied num
 */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - divides a num
+ * @a: first dividend
+ * @b: second dividend
+ * Return: divided num
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error");
-		exit (100);
+		printf("Error\n");
+		exit(100);
 	}
-	return (a/b);
+	return (a / b);
 }
 /**
-* op_mod -
-* @a:
-* @b:
-* Return: a % b
+* op_mod - modulo of num
+* @a: num
+* @b: modulo number
+* Return: reminder
 */
 int op_mod(int a, int b)
 {
