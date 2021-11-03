14ff5cf1675cc18787c8ebfc13eb21d14569b45c
salvador <3940@holbertonschool.com>
2021-11-03 08:17:09 -0700

    fixed ex 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,12 @@
 
 /**
  * print_name - prints a name.
+ *
  * @name: name to print
  * @f: pointer to function that prints
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != 0 && name != NULL && f)
-		(*f)(name);
+	if (f)
+		f(name);
 }
