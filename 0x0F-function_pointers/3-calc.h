bc87d99b4bf0b937c392ebc906eb068325e950c4
salvador <3940@holbertonschool.com>
2021-11-03 16:38:04 -0700

    a

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 
 #define MAIN_H
-#include <stddef.h>
-int (*get_op_func(char *s))(int, int);
 /**
  * struct op - Struct op
  *
@@ -13,8 +11,9 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-/* alias for struct op */
 } op_t;
+
+int (*get_op_func(char *s))(int, int);
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
