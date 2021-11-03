d211abd908bf832555f8a54eb52570efd958b37e
salvador <3940@holbertonschool.com>
2021-11-03 08:39:20 -0700

    fixed ex 1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array || cmp)
-		return (-1);
-	for (i = 0; i < size; i++)
-	{
-		/*if function pointed by cmp returns 1 */
-		if (cmp(array[i]))
-			return (i);
+	if (array && cmp)
+	{	
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			/*if function pointed by cmp returns 1 */
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
 	return (-1);
 }
