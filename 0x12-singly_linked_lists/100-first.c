aa24dd2eb4baeb221eeabae870724f36c275a27b
salvador <3940@holbertonschool.com>
2021-11-11 17:00:46 -0800

    added ex 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - prints string before main()
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
