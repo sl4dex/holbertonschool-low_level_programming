18b2fed5dedebe856d90523971bee5adf94d4543
salvador <3940@holbertonschool.com>
2021-11-16 12:49:12 -0800

    fixing ex 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: head of list
@@ -20,15 +20,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (!new)
 			return (NULL);
 		new->n = n;
-		if (idx)
+		if (!idx)
 		{
-			for (i = 0; i != idx - 1; i++)
-			{
-				if (last->next)
-					last = last->next;
-				else
-					return (NULL);
-			}
+			new->next = *head;
+			*head = new;
+			return (*head);
+		}
+		for (i = 0; i != idx - 1; i++)
+		{
+			if (last->next)
+				last = last->next;
+			else
+				return (NULL);
 		}
 		new->next = last->next;
 		last->next = new;
