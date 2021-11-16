0ab85f5018ef2d59f4a5e9ac6651fede81cc30e5
salvador <3940@holbertonschool.com>
2021-11-16 08:10:57 -0800

    added ex 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets head to NULL
+ * @head: address of pointer to start of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *last = *head;
+
+	if (head)
+	{
+		if (*head)
+		{
+			if (last->next)
+				free_listint2(&(last->next));
+			free(last);
+		}
+		*head = NULL;
+	}
+}
