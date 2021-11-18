8003507a000673d471ec8628035dbfe2df4981f0
salvador <3940@holbertonschool.com>
2021-11-18 09:04:06 -0800

    fixing ex 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,20 +11,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int bnum, dnum, n, pos;
-	
+
 	dnum = 0;
 	if (!b)
-		return(0);
+		return (0);
 	for (pos = 0; b[pos] != 0; pos++)
-	{	
+	{
 		if (b[pos] != '0' && b[pos] != '1')
-			return(0);
+			return (0);
 	}
 	bnum = atoi(b);
 	for (pos = 0; bnum != 0; pos++)
 	{
 		n = bnum % 10;
-		dnum += n<<pos;
+		dnum += n << pos;
 		bnum /= 10;
 	}
 	return (dnum);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 unsigned int binary_to_uint(const char *b);
-unsigned int _pow(unsigned int base, int k);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
