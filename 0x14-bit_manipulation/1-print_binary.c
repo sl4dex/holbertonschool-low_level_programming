4c944304aab9018afd21befe519959aa9e71e7b8
salvador <3940@holbertonschool.com>
2021-11-18 11:00:50 -0800

    fixing ex 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,17 +10,18 @@ void print_binary(unsigned long int n)
 	for (i = 0; n >> i != 0; i++)
 	{
 	}
-	if (i == 0)
-		_putchar(i + '0');
+	if (n == 0)
+		_putchar('0');
 	else
 	{
 		i--;
-		do {
+		while (i >= 0)
+		{
 			if (n >> i & 1)
 				_putchar('1');
 			else
 				_putchar('0');
-		i--;
-		} while (i >= 0);
+			i--;
+		}
 	}
 }
