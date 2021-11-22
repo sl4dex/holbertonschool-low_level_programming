dc15b3fbb55c61a42f5b3b3d5233102a67f88a56
salvador <3940@holbertonschool.com>
2021-11-22 12:41:56 -0800

    fixing ex 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /**
  * create_file - creates a file.
  * @filename: name of the file to create
@@ -13,13 +14,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content == NULL)
 		outw = write(fd, "", 1);
 	else
-		outw = write(fd, text_content, strlen(text_content) + 1);
+		outw = write(fd, text_content, strlen(text_content));
 	if (outw == -1)
 		return (-1);
 	return (1);
