c311cb3799851c1ec174402f605b8cefb5055e20
salvador <3940@holbertonschool.com>
2022-01-07 02:31:29 -0800

    created workspace, added ex 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: head of dll
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	if (h)
+	{
+		printf("%d\n", h->n);
+		size++;
+		while (h->next)
+		{
+			h = h->next;
+			printf("%d\n", h->n);
+			size++;
+		}
+	}
+	return (size);
+}
