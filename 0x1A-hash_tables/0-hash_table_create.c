c04f5832a4befca826f61c93b319fc8618748b28
sl4 <s4lv4dord@gmail.com>
2022-01-28 07:38:56 -0800

    fixing ex 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!size)
 		return (NULL);
 	hash = malloc(sizeof(hash_table_t));
+	if (!hash)
+		return (NULL);
 	hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(hash->array))
-	{
-		free(hash);
 		return (NULL);
-	}
 	hash->size = size;
 	return (hash);
 }
