f3f95b6867447f02de67396901e112904f126a74
sl4 <s4lv4dord@gmail.com>
2022-01-28 08:32:23 -0800

    fixing ex 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int idx;
 	hash_node_t *current;
-
+	
+	if (!ht || !key)
+		return (NULL);
 	idx = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[idx];
 	while (current)
